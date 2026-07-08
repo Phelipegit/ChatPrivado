42b75b434a88bbcf85a2adbcc923578d857a5f97
Phelipe Bistaffa <phelipegithub@gmail.com>
2026-07-08 16:23:49 -0400

    token log

--- ChatPrivadoFront/src/Login.jsx
+++ ChatPrivadoFront/src/Login.jsx
@@ -10,6 +10,7 @@ export default function Login() {
 
     useEffect(() => {
       const token = localStorage.getItem("token");
+      console.log(token);
       if(token) {
         window.location.href= "/home";
       }
