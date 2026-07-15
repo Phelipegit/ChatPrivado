e8bde79b01ca5d379b57447f820614aa58e25bf0
Phelipe Bistaffa <phelipegithub@gmail.com>
2026-07-14 23:15:44 -0400

    fix : updatePassword

--- ChatPrivadoBack/src/main/java/ProjetoPhelipe/ChatPrivado/service/RedefinicaoSenhaService.java
+++ ChatPrivadoBack/src/main/java/ProjetoPhelipe/ChatPrivado/service/RedefinicaoSenhaService.java
@@ -22,11 +22,17 @@ public class RedefinicaoSenhaService {
 
     public ResponseEntity<UserUpdatePasswordResponse> redefinirSenha(UserUpdatePasswordRequest request) {
 
+        Boolean existKeyEmail = redisTemplate.hasKey(request.getEmail().trim().toLowerCase());
+
+        if(existKeyEmail) {
+            return ResponseEntity.status(429).body(new UserUpdatePasswordResponse("E-mail já enviado, tente novamente em alguns minutos",false));
+        }
+
         String token = UUID.randomUUID().toString();
 
-        redisTemplate.opsForValue().set(token,request.getEmail().trim().toLowerCase(), Duration.ofMinutes(10));
+        redisTemplate.opsForValue().set("reset-password:" + request.getEmail().trim().toLowerCase(),token, Duration.ofMinutes(10));
 
-        String url = "https://app.phelipedev.com.br/updatePassword/" + token;
+        String url = "https://app.phelipedev.com.br/updatePassword/" + token + "?email=" + request.getEmail().trim().toLowerCase();
 
         String html = "<h1>Redefinição de Senha</h1>" +
                 "<p>Seu link expira em 10 minutos </p>" +
@@ -34,6 +40,6 @@ public class RedefinicaoSenhaService {
 
         emailService.enviarEmail(request.getEmail().trim().toLowerCase(),"Redefinição de Senha",html);
 
-        return ResponseEntity.ok().body(new UserUpdatePasswordResponse(null,true));
+        return ResponseEntity.ok().body(new UserUpdatePasswordResponse("Um link para redefinir a senha foi enviado para seu e-mail",true));
     }
 }
--- ChatPrivadoFront/src/SendEmailPassword.jsx
+++ ChatPrivadoFront/src/SendEmailPassword.jsx
@@ -1,44 +1,47 @@
 import { useState } from "react"
 export default function SendEmailPassword() {
     const API_URL = "https://api.phelipedev.com.br";
-    const [email,setEmail] = useState("");
-    const [message,setMessage] = useState("");
-    const [success,setSucess] = useState(false);
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [success, setSucess] = useState(false);
 
     async function enviarEmailToken() {
-        
-        if(email.trim() == "") {
+
+        if (email.trim() == "") {
             setMessage("Insira um e-mail válido");
             return;
         }
-        
-        const response = await fetch(`${API_URL}/auth/password/sendEmail`,{
-            method:"POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify({"email":email})
-        });
+        try {
+            const response = await fetch(`${API_URL}/auth/password/sendEmail`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ "email": email })
+            });
 
-        console.log(JSON.stringify({"email":email}));
+            const data = await response.json();
 
-        const data = await response.json();
+            setMessage(data.response);
 
-        
-
-        if(data.success) {
-            setMessage("Um link para redefinir a senha foi enviado para seu e-mail");
-            setSucess(true);
+            if (data.success) {
+                setSucess(true);
+            } else {
+                setSucess(false);
+            }
+        }catch(err) {
+            setSucess(false);
+            setMessage(err);
         }
     }
 
     return (
-        <div  id="container-principal" style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+        <div id="container-principal" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
             <h1>Redefinição de senha</h1>
             <strong>Insira seu e-mail</strong>
             <label>E-mail</label>
-            <input placeholder="seu@email.com" onChange={(a => setEmail(a.target.value))}/>
-            <p style={{color: success ? "green" : "red"}}>{message}</p>
+            <input placeholder="seu@email.com" onChange={(a => setEmail(a.target.value))} />
+            <p style={{ color: success ? "green" : "red" }}>{message}</p>
             <button onClick={() => enviarEmailToken()}>Redefinir senha</button>
         </div>
     )
